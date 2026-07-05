5752b3d4d1ca9306069b0b26fa6d66d92141314f
toben1010 <eminoz2010@gmail.com>
2026-07-05 21:08:39 +1000

    Fix: Revert all changes back to their initial state

--- frontend/src/preload.ts
+++ frontend/src/preload.ts
@@ -1,8 +1,2 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('overlayApi', {
-  openOverlay: (name: string) => ipcRenderer.send('open-overlay', name),
-});
