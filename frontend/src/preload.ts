eed0b86b1d9354eb08b92ec749b67d0a29f15964
Yolanda Chai <z5688901@unsw.edu.au>
2026-07-06 03:42:50 +1000

    Feat: Add window behaviour and z-value behaviour for menu and overlays

--- frontend/src/preload.ts
+++ frontend/src/preload.ts
@@ -5,4 +5,11 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('overlayApi', {
   openOverlay: (name: string) => ipcRenderer.send('open-overlay', name),
+  closeOverlay: (name: string) => ipcRenderer.send('close-overlay', name),
+  setIgnoreMouseEvents: (ignore: boolean) => ipcRenderer.send('set-ignore-mouse-events', ignore),
+  focusMenu: () => ipcRenderer.send('focus-menu'),
+  closeMenu: () => ipcRenderer.send('close-menu'),
+  onMenuOpenChanged: (callback: (isOpen: boolean) => void) => {
+    ipcRenderer.on('menu-open-changed', (_event, isOpen: boolean) => callback(isOpen));
+  },
 });
