c56a246943a007d9270257d25dc1f1c0dfb59aff
Yolanda Chai <z5688901@unsw.edu.au>
2026-07-06 08:42:06 +1000

    Fix: Add preload scripts and APIs

--- frontend/src/overlay-api.d.ts
+++ frontend/src/overlay-api.d.ts
@@ -3,10 +3,13 @@ declare global {
     overlayApi: {
       openOverlay: (name: string) => void;
       closeOverlay: (name: string) => void;
+      toggleOverlay: (name: string) => void;
+      getOverlayStates: () => Promise<Record<string, boolean>>;
       focusMenu: () => void;
       closeMenu: () => void;
       setIgnoreMouseEvents: (ignore: boolean) => void;
       onMenuOpenChanged: (callback: (isOpen: boolean) => void) => void;
+      onOverlayStateChanged: (callback: (name: string, isOpen: boolean) => void) => void;
     };
   }
 }
--- frontend/src/preload.ts
+++ frontend/src/preload.ts
@@ -6,10 +6,15 @@ import { contextBridge, ipcRenderer } from 'electron';
 contextBridge.exposeInMainWorld('overlayApi', {
   openOverlay: (name: string) => ipcRenderer.send('open-overlay', name),
   closeOverlay: (name: string) => ipcRenderer.send('close-overlay', name),
+  toggleOverlay: (name: string) => ipcRenderer.send('toggle-overlay', name),
+  getOverlayStates: () => ipcRenderer.invoke('get-overlay-states'),
   setIgnoreMouseEvents: (ignore: boolean) => ipcRenderer.send('set-ignore-mouse-events', ignore),
   focusMenu: () => ipcRenderer.send('focus-menu'),
   closeMenu: () => ipcRenderer.send('close-menu'),
   onMenuOpenChanged: (callback: (isOpen: boolean) => void) => {
     ipcRenderer.on('menu-open-changed', (_event, isOpen: boolean) => callback(isOpen));
+  },
+  onOverlayStateChanged: (callback: (name: string, isOpen: boolean) => void) => {
+    ipcRenderer.on('overlay-state-changed', (_event, name: string, isOpen: boolean) => callback(name, isOpen));
   },
 });
